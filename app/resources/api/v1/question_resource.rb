8710f79486aa011abd3f02cb7a6f56bec090721f
Hemant <hemantdev.hts@gmail.com>
2017-08-29 19:33:14 +0530

    Fix indentation changes

--- app/resources/api/v1/question_resource.rb
+++ app/resources/api/v1/question_resource.rb
@@ -1,9 +1,9 @@
 module Api
-	module V1
-		class QuestionResource < JSONAPI::Resource
-		  attributes :description, :marks, :time_of_question, :options, :correct_answer
+  module V1
+    class QuestionResource < JSONAPI::Resource
+      attributes :description, :marks, :time_of_question, :options, :correct_answer
       has_one :quiz
       has_many :answers
     end
-	end	
+  end	
 end
