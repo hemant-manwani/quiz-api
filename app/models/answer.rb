424262c518e68e7a461642b315cee55c59253a97
Hemant <hemantdev.hts@gmail.com>
2017-08-29 20:53:48 +0530

    Add quiz timeout function and validations

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,11 @@ class Answer < ApplicationRecord
 
   # validation
   validates :given_answer, :question, :answer_set, presence: true
+  validate :quiz_timeout
+  validates_uniqueness_of :question, scope: :answer_set
+
+  def quiz_timeout
+    time = Time.now - answer_set.quiz.created_at
+    errors.add(:base, "Quiz timeout") if time > answer_set.quiz.questions.sum(:time_of_question)*60
+  end
 end
